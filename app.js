221a16c3dc5974e024e584451fc86b96cb9351b8
NishiGaba <gabanishi45@gmail.com>
2018-02-21 14:47:49 +0530

    Server Created and Depencies Added

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const users = require('./routes/users');
+
+//PORT NUMBER
+const port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+//Index Route
+app.get('/',(req,res) => {
+	res.send('Invlid Endpoint');
+});
+
+//Start Server
+app.listen(port, () => {
+	console.log('Server started on port '+ port);
+});
